8b6f8ea6866c62fa4ec2b009690e344c4c3d1e34
julianechat <juliane.vigneault@umontreal.ca>
2023-03-14 16:07:45 -0400

    Update ModelAnalysis.R

---
+++ scripts/ModelAnalysis.R
@@ -0,0 +1,20 @@
+# ---- Script Setup ----
+## R Setup ----
+
+to.data <- "./data/"
+to.script <- "./scripts/"
+to.output <- "./output/"
+to.figs <- "./figs/"
+to.R <- "./R/"
+to.carto <- "./carto/"
+
+## Loading packages ----
+
+## Loading data ----
+
+
+# ---- Data exploration ----
+## Outliers ----
+## Collinearity ----
+## Relationships ----
+
